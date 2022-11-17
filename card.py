1d516cab894ecc9b154c18cfe5b8353bf2ed0e7a
aliciakenneally <alicia.kenneally@gmail.com>
2022-11-16 19:09:55 -0500

    created methods under Deck class

--- card.py
+++ card.py
@@ -2,24 +2,24 @@ import random
 
 
 class Card:
-    value = None
-    suit = None
+    value =
+    suit =
 
     def __init__(self, value, suit):
         self.cardValue = value
         self.cardSuit = suit
 
-    def __str__(self):
-        return 'Card Value: {}, Card Suit: {}'.format(self.value, self.suit)
-
 
 class Deck:
     def __init__(self):
         self.cards = []
-        self.create_deck
-        self.shuffle_deck
-        self.get_card
+
+    def shuffle_deck(self):
+        random.shuffle(self.cards)
+
+    def create_deck(self):
+
+    def get_card(self):
 
 
-card = Card("Card", 3)
-card.__str__()
+
